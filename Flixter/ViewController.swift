7388691b8f73d01bdd0ad932e0d0bf291fce9e95
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2018-11-16 22:12:21 -0500

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Jorge Alejandre on 11/16/18.
+//  Copyright © 2018 Jorge Alejandre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
