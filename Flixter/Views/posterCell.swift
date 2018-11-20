d86b27d75a1321858da046e540551bb012b621fc
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2018-11-20 00:45:47 -0500

    Need Fixing and Editing

---
+++ Flixter/Views/posterCell.swift
@@ -0,0 +1,14 @@
+//
+//  posterCell.swift
+//  Flixter
+//
+//  Created by Jorge Alejandre on 11/19/18.
+//  Copyright © 2018 Jorge Alejandre. All rights reserved.
+//
+
+import UIKit
+
+class posterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
