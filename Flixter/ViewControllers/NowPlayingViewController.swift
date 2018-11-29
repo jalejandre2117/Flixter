195114d77877716f0b89029191e1aa3ba623cd2f
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2018-11-29 14:26:21 -0500

    need optional stories

--- Flixter/ViewControllers/NowPlayingViewController.swift
+++ Flixter/ViewControllers/NowPlayingViewController.swift
@@ -24,7 +24,8 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
-        tableView.rowHeight = 200
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 50
         
         
         fetchMovies()
